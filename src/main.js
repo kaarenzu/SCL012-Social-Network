734e54c5e9796662e1c043e2258140925355e174
Corina <corinacvp@gmail.com>
2020-01-17 10:20:23 -0300

    configuracion de objetos

--- src/main.js
+++ src/main.js
@@ -41,3 +41,32 @@ acceder.addEventListener('click', () => {
 		});
 
 });
+
+function observador(){
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			console.log('existe usuario activo');
+			aparece();
+		  // User is signed in.
+		  var displayName = user.displayName;
+		  var email = user.email;
+		  var emailVerified = user.emailVerified;
+		  var photoURL = user.photoURL;
+		  var isAnonymous = user.isAnonymous;
+		  var uid = user.uid;
+		  var providerData = user.providerData;
+		  // ...
+		} else {
+			console.log('no existe usuario activo');
+		  // User is signed out.
+		  // ...
+		}
+	  });email-password.html
+}
+observador();
+
+function aparece(){
+	let contenido=document.getElementById('root');
+	contenido.innerHTML=
+	"Aparece contenido cuando inicia sesión </div>"
+}
