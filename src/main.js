7ca35f76c572fc9d646c67a16fe932b18fa90d56
Corina <corinacvp@gmail.com>
2020-01-18 23:21:13 -0300

    vista y validaciones create usuario

--- src/main.js
+++ src/main.js
@@ -3,28 +3,23 @@
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
-let contenido=document.getElementById('root');
+let contenido = document.getElementById('root');
+function mostrarLogin(){
+	contenido.innerHTML =
+					`<div>
+      <img src="img/logo tech.png"class="logo" >
+     </div>
+     <div class="login">
+    	<h1>Inicia Sesión</h1>
+    	<input type="email" name="" id="email2" placeholder="Usuario o correo electrónico" class="input">
+    	<input type="password" name="" id="password2" placeholder="**************" class="input">
+		<button id="ingresar" class="btn">Ingresar</button>
+		<button id="gmail" class="btn2">Gmail</button>
+		<h2 > ¿Olvidaste tu contraseña?</h2><a  href="#" class="recuperar">Recupérala Aquí</a>
+		<h3> Crea tu cuenta</h3> <a href="#"class="aquí">Aqui</a>
+    </div>`;
+}
 
-//Registro de Usuarios
-// let btnEnviar = document.getElementById('enviar')
-
-// btnEnviar.addEventListener('click', () => {
-// 	let email = document.getElementById('email').value;
-// 	let password = document.getElementById('password').value;
-// 	firebase.auth().createUserWithEmailAndPassword(email, password)
-// 		.then((response) => {
-// 			console.log(response);
-// 		})
-// 		.catch(function (error) {
-// 			// Handle Errors here.
-// 			var errorCode = error.code;
-// 			var errorMessage = error.message;
-
-// 			console.log(errorCode);
-// 			console.log(errorMessage); 
-// 		});
-
-// });
 //Ingreso de Usuarios
 let acceder = document.getElementById('ingresar');
 
@@ -42,30 +37,73 @@ ingresar.addEventListener('click', () => {
 		});
 
 });
+//<-------------Link crea tu cuenta aquí-------------->
+document.getElementById('crearCuenta').addEventListener('click', () => {
+	contenido.innerHTML = '';
+	contenido.innerHTML =
+		`<div>
+    <img src="img/logo tech.png"class="logo" >
+    </div>
+    <div class="login">
+		<h1>Crea tu cuenta</h1>
+		<form>
+		<input type="text" name="" placeholder="Nombre" class="input" requiere>
+    	<input type="text" name=""  placeholder="Apellido" class="input"requiere>
+    	<input type="email" name="" id="email" placeholder="Usuario o correo electrónico" class="input"requiere>
+    	<input type="password" name="" id="password" placeholder="**************" class="input"requiere>
+		<p>Contraseña debe tener mínimo 8 caracteres.</p>
+		<p>Campos con * son obligatorios.</p>
+		<button id="registrarse" class="btn">Registrarse</button>
+		</form>
+	</div>`;
+	
+//<-------------Botón Registrarse-------------->
+	document.getElementById('registrarse').addEventListener('click', () => {
+		let email = document.getElementById('email').value;
+		let password = document.getElementById('password').value;
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+			.then((response) => {
+				alert('Su usuarios ha sido creado correctamente')
+				aparece();
+				console.log(response);
+			})
+			.catch(function (error) {
+				alert('Su usuarios no ha sido creado correctamente, por favor intentalo nuevamente')
+				mostrarLogin();
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
 
-function observador(){
-	firebase.auth().onAuthStateChanged(function(user) {
+				console.log(errorCode);
+				console.log(errorMessage);
+			});
+	})
+})
+
+
+function observador() {
+	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
 			console.log('existe usuario activo');
 			aparece();
-		  // User is signed in.
-		  var displayName = user.displayName;
-		  var email = user.email;
-		  var emailVerified = user.emailVerified;
-		  var photoURL = user.photoURL;
-		  var isAnonymous = user.isAnonymous;
-		  var uid = user.uid;
-		  var providerData = user.providerData;
-		  // ...
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var isAnonymous = user.isAnonymous;
+			var uid = user.uid;
+			var providerData = user.providerData;
+			// ...
 		} else {
 			console.log('no existe usuario activo');
 		}
-	  })
+	})
 }
 
 observador();
-function aparece(){
-	contenido.innerHTML= `
+function aparece() {
+	contenido.innerHTML = `
     <header>
         <nav>
         <img src = "img/logoblanco.png" class= "imagenes">
@@ -80,12 +118,12 @@ function aparece(){
         </nav> 
     </header>
 	`;
-//<-------------Función botón Cerrar Sesión-------------->
-document.querySelector('#cerrarSesion').addEventListener('click', () => {
-firebase.auth().signOut()
-.then(function(){
-contenido.innerHTML = 
-	`<div>
+	//<-------------Función botón Cerrar Sesión-------------->
+	document.querySelector('#cerrarSesion').addEventListener('click', () => {
+		firebase.auth().signOut()
+			.then(function () {
+				contenido.innerHTML =
+					`<div>
       <img src="img/logo tech.png"class="logo" >
      </div>
      <div class="login">
@@ -98,10 +136,10 @@ contenido.innerHTML =
 		<h3> Crea tu cuenta</h3> <a href="#"class="aquí">Aqui</a>
     </div>`;
 
-	console.log('Saliendo...')
-})
-.catch(function(error){
-	console.log(error);
-})
-})
+				console.log('Saliendo...')
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+	})
 };
