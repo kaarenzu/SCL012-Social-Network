227674ae902c9e288f99aff554324b7b3f66ef91
camila-osores <camilaosoresa@gmail.com>
2020-01-20 16:55:49 -0300

    cambios en css de login y muro

--- src/main.js
+++ src/main.js
@@ -11,13 +11,13 @@ function mostrarLogin() {
 	  	<div class="login">
 		<h1>Inicia sesión</h1>
 		<form>
-		  <input type="email" name="" id="email2" placeholder="  Usuario o correo electrónico" class="input" required>
-		  <input type="password" name="" id="password2" placeholder="  **************" class="input" required>
+		  <input type="email" name="" id="email2" placeholder=" Correo electrónico" class="input" required>
+		  <input type="password" name="" id="password2" placeholder=" **************" class="input" required>
 		  <button id="ingresar" class="btn">Ingresar</button>
 		</form>
 		<button id="gmail" class="btn2">Gmail</button>
-		<h2>¿Olvidaste tu contraseña? </h2> <a href="#" class="recuperar"> Recupérala Aquí</a>
-		<h3> Crea tu cuenta</h3> <a id="crearCuenta" class="aqui">Aquí</a>
+		<h2>¿Olvidaste tu contraseña? <a href="#" class="recuperar">Recupérala Aquí</a></h2> 
+		<h3> Crea tu cuenta <a id="crearCuenta" class="aqui">Aquí</a></h3> 
 	  </div>`;
 }
 //<-------------Inicia Sesión-------------->
@@ -46,14 +46,14 @@ document.getElementById('crearCuenta').addEventListener('click', () => {
 	contenido.innerHTML = '';
 	contenido.innerHTML =
 		`<div>
-    <img src="img/logo tech.png"class="logo" >
+    <img src="img/logo tech.png"class="logo">
     </div>
     <div class="login">
 		<h1>Crea tu cuenta</h1>
 		<form>
 		<input type="text" name="" placeholder="Nombre" class="input" requiere>
     	<input type="text" name=""  placeholder="Apellido" class="input"requiere>
-    	<input type="email" name="" id="email" placeholder="Usuario o correo electrónico" class="input"requiere>
+    	<input type="email" name="" id="email" placeholder="Correo electrónico" class="input"requiere>
     	<input type="password" name="" id="password" placeholder="**************" class="input"requiere>
 		<p>Contraseña debe tener mínimo 8 caracteres.</p>
 		<p>Campos con * son obligatorios.</p>
@@ -73,7 +73,7 @@ document.getElementById('crearCuenta').addEventListener('click', () => {
 				alert('Su usuario ha sido creado correctamente, por favor verifica tu bandeja de entrada en tu email')
 			})
 			.catch(function (error) {
-				alert('Upps!! Su usuario no ha sido creado correctamente, por favor intentalo nuevamente')
+				alert('Upps!! Su usuario no ha sido creado correctamente, por favor inténtalo nuevamente')
 				// Handle Errors here.
 				var errorCode = error.code;
 				var errorMessage = error.message;
@@ -113,31 +113,35 @@ function mostrarHome(user) {
 		contenido.innerHTML = `
 		<header>
 			<nav>
-			<img src = "img/logoblanco.png" class= "imagenes">
+			<img src ="img/logoblanco.png" class="imagenes">
 				<ul>
-					<li><a href= "#"class= "btnMenu">Inicio </a> </li>
-					<li><a href= "#"class= "btnMenu">Computación</a></li>
-					<li><a href= "#"class= "btnMenu"> Videojuegos</a></li>
-					<li><a href= "#"class= "btnMenu">Accesorios</a></li>
-					<li><a href= "#" class= "btnMenu">Publica tus ventas</a> </li>
-					<img src = "img/cerrablanco.png" class= "cerrar"id="cerrarSesion">
+					<li><a href= "#"class="btnMenu">Inicio </a> </li>
+					<li><a href= "#"class="btnMenu">Computación</a></li>
+					<li><a href= "#"class="btnMenu"> Videojuegos</a></li>
+					<li><a href= "#"class="btnMenu">Accesorios</a></li>
+					<li><a href= "#" class="btnMenu">Publica tus ventas</a> </li>
+					<img src ="img/cerrablanco.png" class="cerrar"id="cerrarSesion">
 				</ul>
 			</nav> 
 		</header>
-			<div class= "contenedor"> 
-		<div class= "icono">
-		<img src = "img/icono-imagen.png" class= "iconos">
-		 </div>
-		<div class= "poster">
-		<input type="text" id="post" class="post" >
-		 </div>
-		 <div class= "like">
-		 <img src = "img/like.png" class= "like" id= "like">
-		 </div>
-		 <div class= "comentario">
-		 <img src = "img/comment.png" class= "comentar" id= "comentar">
-		 </div>                                                     
-		</div>		
+
+			<div class="contenedor"> 
+		<div>
+		<img src="img/icono-imagen.png" class="iconos">
+		</div>
+
+		<div>
+		<input type="text" id="post" class="post">
+		</div>
+
+		<div>
+		<img src="img/like.png" class="like" id="like">
+		</div>
+
+		<div>
+		<img src="img/comment.png" class="comentar" id="comentar">
+		</div>                                                     
+			</div>		
 		
 		`;
 		//<-------------Función botón Cerrar Sesión-------------->
