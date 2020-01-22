2ad84d429c3299db27510259ebebfe74b9809fa6
Corina <corinacvp@gmail.com>
2020-01-22 19:25:49 -0300

    cambios

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
 import { signIn, createUser, ingresarGoogle} from './lib/index.js';
 
 let contenido = document.getElementById('root');
-loginPage();
-function loginPage() {
-	contenido.innerHTML = `	
-	<div>
-		<img src="img/logo tech.png" class="logo">
-	  </div>
-	  <div class="login">
-		<h1>Inicia sesión</h1>
-		<form>
-			  <input type="email" name="" id="email2" placeholder=" Correo electrónico" class="input" required>
-			  <input type="password" name="" id="password2" placeholder=" **************" class="input" required>
-			  <button id="ingresar" class="btn">Ingresar</button>
-		</form>
-			<button id="gmail" class="btn2">Gmail</button>
-			<a href="#" class="recuperar">¿Olvidaste tu contraseña? Recupérala Aquí</a>
-			  <a href="#" id="crearCuenta" class="aqui">Crear cuenta aquí</a> 
-	  </div>`;
-};
+mostrarLogin();
 
 function mostrarLogin() {
 	window.location.hash = '/Inicio';
@@ -46,6 +29,8 @@ document.getElementById('ingresar').addEventListener('click', (e) => {
 	e.preventDefault();
 	signIn(email2, password2);
 });
+//<-------------Ingresar con Google-------------->
+document.getElementById('gmail').addEventListener('click', ingresarGoogle);
 //<-------------Link crea tu cuenta aquí-------------->
 document.getElementById('crearCuenta').addEventListener('click', () => {
 	contenido.innerHTML = '';
@@ -166,6 +151,4 @@ function mostrarHome(user) {
 	}
 };
 
-// Ingresar con google 
 
-document.getElementById('gmail').addEventListener('click', ingresarGoogle);
