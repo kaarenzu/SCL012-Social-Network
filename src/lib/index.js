b057335d1c50b71f4af2aaeb08cd8a872bb3f840
kaarenzu <karenzcortes0211@gmail.com>
2020-01-22 13:39:47 -0300

    Corregimos el error de gmail

--- src/lib/index.js
+++ src/lib/index.js
@@ -41,50 +41,29 @@ function verificar() {
 			console.log('error');
 		});
 };
-
-// Autentificacion con google 
+// autentificacion con google 
 export function ingresarGoogle() {
 	var provider = new firebase.auth.GoogleAuthProvider();
 	provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-	firebase.auth().languageCode = 'pt';
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
-provider.setCustomParameters({
-	'login_hint': 'user@example.com'
-  });
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-	// This gives you a Google Access Token. You can use it to access the Google API.
-	var token = result.credential.accessToken;
-	// The signed-in user info.
-	var user = result.user;
-	// ...
-  }).catch(function(error) {
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	// The email of the user's account used.
-	var email = error.email;
-	// The firebase.auth.AuthCredential type that was used.
-	var credential = error.credential;
-	// ...
-  });
-  firebase.auth().signInWithRedirect(provider);
-  firebase.auth().getRedirectResult().then(function(result) {
-	if (result.credential) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // ...
-	}
-	// The signed-in user info.
-	var user = result.user;
-  }).catch(function(error) {
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	// The email of the user's account used.
-	var email = error.email;
-	// The firebase.auth.AuthCredential type that was used.
-	var credential = error.credential;
-	// ...
-  });		
+	firebase.auth().signInWithRedirect(provider);
+	firebase.auth().getRedirectResult().then(function(result) {
+		if (result.credential) {
+		  // This gives you a Google Access Token. You can use it to access the Google API.
+		  var token = result.credential.accessToken;
+		  // ...
+		}
+		// The signed-in user info.
+		var user = result.user;
+	  }).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// The email of the user's account used.
+		var email = error.email;
+		// The firebase.auth.AuthCredential type that was used.
+		var credential = error.credential;
+		// ...
+	  });
+	
+	
 };
